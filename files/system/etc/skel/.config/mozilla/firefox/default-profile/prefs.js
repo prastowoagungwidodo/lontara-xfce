c372564360dc402ea08bb405b38f16035647085d
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-08-17 09:30:38 +0700

    Add default firefox profile, autostart redshift, fix: xdg-portal-desktop, fix: vicinae startup, add ghostty and xfce4-terminal default config

---
+++ files/system/etc/skel/.config/mozilla/firefox/default-profile/prefs.js
@@ -0,0 +1,8 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.compactmode.show", true);
+user_pref("browser.theme.dark-private-windows", false);
+user_pref("browser.translations.automaticallyPopup", false);
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"vertical-spacer\",\"home-button\",\"urlbar-container\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"reset-pbm-toolbar-button\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\"],\"vertical-tabs\":[],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"reset-pbm-toolbar-button\",\"developer-button\",\"screenshot-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"vertical-tabs\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\"],\"currentVersion\":24,\"newElementCount\":4}");
+user_pref("browser.uidensity", 1);
+user_pref("widget.gtk.overlay-scrollbars.enabled", false);
+
