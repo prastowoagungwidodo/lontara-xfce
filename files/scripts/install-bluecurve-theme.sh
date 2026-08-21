6edb5b9fc3de85d38a71ddab131d6bdc04dae327
Prastowo Agung Widodo <prastowoagungwidodo@gmail.com>
2026-08-21 21:17:39 +0700

    Add Bluecurve theme

---
+++ files/scripts/install-bluecurve-theme.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Get latest version of the Bluecurve theme from github
+# https://github.com/neeeeow/Bluecurve/releases
+echo "Installing Bluecurve theme"
+cd /tmp
+echo "Cloning Bluecurve repository"
+git clone https://github.com/neeeeow/Bluecurve.git
+cd Bluecurve
+mkdir build
+cd build
+echo "Building Bluecurve theme"
+cmake ..
+make
+echo "Build complete"
+echo "Installing Bluecurve theme"
+sudo make install
+
+echo "Installing Luxi fonts"
+cd ../fonts
+sudo mkdir -p /usr/share/fonts/luxi
+sudo cp *.ttf /usr/share/fonts/luxi/
+
+echo "Bluecurve theme installed successfully"
+echo "Cleaning up"
+rm -rf /tmp/Bluecurve
+echo "Done."
